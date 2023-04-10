4c393873b70148dfe8b310d06f1a1c166e23681f
Bless-dem <blessdem20@gmail.com>
2023-04-10 00:07:58 -0700

    0 what is my name task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - a program that prints the name, followed by a new line
+ * @argc: counts the total number of arguments
+ * @argv: the array of each number of arguments passed to the program
+ *
+ * Return: 0 (success)
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

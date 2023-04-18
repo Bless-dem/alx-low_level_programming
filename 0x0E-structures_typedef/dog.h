bacc9ce149e2f3752aaeef6d77fda4379e9cb42e
Bless-dem <blessdem20@gmail.com>
2023-04-18 14:40:43 -0700

    free dog task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,15 +7,21 @@
  * @age: integer
  * @owner: character string
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int len_str(char *str);
+dog_t *new_dog(char *name, float age, char *owner);
+
+
 #endif

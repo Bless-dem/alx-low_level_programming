58c15d689a89a9b7fdd4a1d31d17ed4db76edf17
Bless-dem <blessdem20@gmail.com>
2023-05-01 14:44:50 -0700

    print listint

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ *free_listint - a function that frees a list
+ *@head: this points to the allocated free memory
+ *Return: 0
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	if (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
+
+

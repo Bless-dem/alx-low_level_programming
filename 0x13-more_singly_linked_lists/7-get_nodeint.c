383e53426868a5b994ecfac2d054a1b1d5b6cc7c
Bless-dem <blessdem20@gmail.com>
2023-05-02 10:08:42 -0700

    get nodeint

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,24 +3,22 @@
 
 /**
  * *get_nodeint_at_index - a function that the nth node of a list
+ * @head: pointer to head of the listint_t
  *@index: at the index nth node
  *Return: if node does not exist, return NULL
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *new_node
-	= (listint_t *)malloc(sizeof(listint_t));
-	count = 0;
+	unsigned int count;
 
-	while (new_node != NULL)
+	count = 0;
+	while (count != index)
 	{
-		if (count == 0);
-		{
-			return (new_node);
-		}
+		head = head->next;
+		if (head == NULL)
+			return (NULL);
 		count++;
-		new_node = new_node->next;
 	}
-	return NULL;
+	return (head);
 }
